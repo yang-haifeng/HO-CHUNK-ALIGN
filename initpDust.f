f3b49dcd7daa287faedfe87f6670fa4f3444f913
Haifeng Yang <hfyangpku@gmail.com>
2017-04-03 21:51:18 -0400

    Failed experiment on Emittion.

--- initpDust.f
+++ initpDust.f
@@ -8,7 +8,7 @@ c     of the dust
 
       implicit none
 
-      real*8 xran,thetb
+      real*8 xran,thetb,currentEps
       real ran2
       integer ii(3),ir,it,ip
 
@@ -24,14 +24,6 @@ c     incident radiation chosen to be unpolarized. For now.
       sup=0.d0
       svp=0.d0
 
-c     sample cosb, sinb, location of the photon in latitude
-      xran=ran2(i1)
-      cosb=1.d0-2.d0*xran
-      sinb=dsqrt(1.d0-cosb**2)
-c     sample location of the photon in longitude
-      xran = ran2(i1)
-      lp=r2p*xran
-
 c     sample cost, sint, direction of the photon in theta
       xran=ran2(i1)
       cost=1.d0-2.d0*xran
@@ -42,10 +34,27 @@ c     sample cosp, sinp, direction of photon in phi
       sinp=dsin(phi)
       cosp=dcos(phi)
 
-c     sample radius of the location
+10    xran = ran2(i1)
+      ir = int(xran*(nrg-1))+1
       xran = ran2(i1)
-      rtot = (xran**(1.d0/3.d0))*rarr(nrg) ! Uniformly pick a location 
-c     rtot = xran*rarr(nrg) ! Uniformly pick a location 
+      it = int(xran*(ntg-1))+1
+      xran = ran2(i1)
+      ip = int(xran*(npg-1))+1
+
+      currentEps=massarr(ir,it,ip)*temparr(ir,it,ip)
+      xran = ran2(i1)
+      if (xran*maxEps>currentEps) then
+        goto 10
+      endif
+
+c     sample cosb, sinb, location of the photon in latitude
+      thetb = 0.5*(thetarr(it)+thetarr(it+1))
+      cosb=dcos(thetb)
+      sinb=dsin(thetb)
+c     sample location of the photon in longitude
+      lp=0.5*(phiarr(ip)+phiarr(ip))
+
+      rtot = 0.5*(rarr(ir)+rarr(ir+1))
       rsq = rtot*rtot
 
       zp=rtot*cosb
@@ -55,23 +64,5 @@ c     rtot = xran*rarr(nrg) ! Uniformly pick a location
       ux=sint*cosp
       uy=sint*sinp
       uz=cost
-
-      call locate(rarr,nrg,nrg,rtot,ir)
-      ii(1)=ir
-      if (ntg.gt.1) then
-         thetb=dacos(cosb)
-         call locate(thetarr,ntg,ntg,thetb,it)
-         ii(2)=it
-      else
-         ii(2)=1
-         it=1
-      endif
-      if (npg.gt.1) then
-         call locate(phiarr,npg,npg,lp,ip)
-         ii(3)=ip
-      else
-         ii(3)=1
-         ip=1
-      endif
 
       end
